25813324a8a11515e350fccd9691c141f17da810
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2024-05-27 22:06:56 +0200

    added tracing methods

--- AirportMSBackend/Program.cs
+++ AirportMSBackend/Program.cs
@@ -11,10 +11,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Logging.AddSentry(options =>
+builder.Logging.AddSentry(o =>
 {
-    options.Dsn = "https://ee3949679b9394adfbd6ec713e7ae710@o4507302947717120.ingest.de.sentry.io/4507329946124368";
-    // Additional configuration options can be set here
+    o.Dsn = "https://ee3949679b9394adfbd6ec713e7ae710@o4507302947717120.ingest.de.sentry.io/4507329946124368";
+    o.Debug = true;
+    // Set TracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    o.TracesSampleRate = 1.0;
+    // Sample rate for profiling, applied on top of othe TracesSampleRate,
+    // e.g. 0.2 means we want to profile 20 % of the captured transactions.
+    // We recommend adjusting this value in production.
+    o.ProfilesSampleRate = 1.0;
+    // Requires NuGet package: Sentry.Profiling
+    // Note: By default, the profiler is initialized asynchronously. This can
+    // be tuned by passing a desired initialization timeout to the constructor.
+    o.AddIntegration(new ProfilingIntegration(
+        // During startup, wait up to 500ms to profile the app startup code.
+        // This could make launching the app a bit slower so comment it out if you
+        // prefer profiling to start asynchronously.
+        TimeSpan.FromMilliseconds(500)
 });
 var app = builder.Build();
 
